053449d4292d42172c31983c933a4d11fdde553a
gradgrind <mt.bothfeld@gmail.com>
2025-06-07 19:29:33 +0200

    Updated various1.minion

--- cpp/test/main.cpp
+++ cpp/test/main.cpp
@@ -48,31 +48,31 @@ const char* callback2(
     throw "Invalid callback";
 }
 
-/*TODO
 const char* callback3(
     const char* data)
 {
     auto m = minion::Reader::read(data);
-    auto mm = m.m_map();
-    string wname;
-    (*mm)->get_string("CALLBACK", wname);
-    {
-        minion::Writer writer(m, 0);
-        printf("callback got '%s'\n", writer.dump_c());
-    }
+    printf("callback got '%s'\n", data);
+    if (auto mm = m.m_list()) {
+        string wname;
+        if ((*mm)->get_string(0, wname)) {
+            minion::MList mlist;
+            if (wname == "EF1") {
+                mlist = minion::MList({"WIDGET", "popup", {"SHOW", wname}});
+            } else {
+                mlist = minion::MList({"WIDGET", "TableTotals", {"VALUE", data}});
+            }
 
-    minion::MMap mp;
-    if (wname == "EF1") {
-        mp = minion::MMap({{"WIDGET", "popup"}, {"DO", {{"SHOW", wname}}}});
-    } else {
-        mp = minion::MMap({{"WIDGET", "TableTotals"}, {"DO", {{"VALUE", data}}}});
+            minion::MValue val{mlist};
+            minion::Writer writer(val, -1);
+            callback_data = writer.dump_c();
+            printf("??? %s\n", callback_data.c_str());
+            fflush(stdout);
+            return callback_data.c_str();
+        }
     }
-    auto cbr = dump(mp);
-    //printf("??? %s\n", cbr);
-    //fflush(stdout);
-    return cbr;
+    throw "Invalid callback";
 }
-*/
 
 int main()
 {
@@ -80,16 +80,16 @@ int main()
         // These paths are realtive to the directory
         // in which the binary is built.
         //"../../examples/buttons1.minion",
-        "../../examples/grid1.minion",
-        //"../../examples/various1.minion"
+        //"../../examples/grid1.minion",
+        "../../examples/various1.minion"
         //
     };
 
     auto flist = {
         //
         //callback1,
-        callback2,
-        //callback3
+        //callback2,
+        callback3
         //
     };
 
