1ee5a4ac3be123b7c9148fa98a857ee2f2a8b906
gradgrind <mt.bothfeld@gmail.com>
2025-06-09 19:54:05 +0200

    Go version of the demo

--- go/examples/demo/main.go
+++ go/examples/demo/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"path/filepath"
+	"strings"
 
 	"github.com/gradgrind/minion/gominion"
 	"github.com/gradgrind/minion_gui/go/mugui"
@@ -10,7 +11,7 @@ import (
 
 func main() {
 	fp, err := filepath.Abs(
-		filepath.Join("..", "..", "..", "examples", "demo1.minion"))
+		filepath.Join("..", "..", "..", "examples", "demo.minion"))
 	if err != nil {
 		panic(err)
 	}
@@ -20,6 +21,9 @@ func main() {
 	mugui.MinionGui(fpm, callback)
 }
 
+var tabs = map[string]bool{}
+
+// TODO
 func callback(data string) string {
 	fmt.Printf("Go callback got '%s'\n", data)
 	v := gominion.ReadMinion(data)
@@ -31,10 +35,83 @@ func callback(data string) string {
 		fmt.Println(gominion.DumpMinion(v, -1))
 	}
 	mm := v.(gominion.MList)
+	var cbr string
 	var wname string
 	mm.GetString(0, &wname)
 
-	cbr := fmt.Sprintf(`[[WIDGET,"viewer",[TEXT,"%s"]]]`, wname)
+	if wname == "()buttons" {
+
+		cbr = "[[WIDGET_PREFIX, buttons]"
+		wtab := "(buttons)l_MainWindow"
+		if !tabs[wtab] {
+			cbr += ",[MINION_FILE, ../../../examples/buttons.minion],[WIDGET, "
+			cbr += wtab
+			cbr += ", [PARENT, ()viewer]]"
+			tabs[wtab] = true
+		}
+		cbr += ",[WIDGET, ()viewer, [SELECT, "
+		cbr += wtab
+		cbr += "]]]"
+
+	} else if wname == "()grid" {
+
+		cbr = "[[WIDGET_PREFIX, grid]"
+		wtab := "(grid)l_MainWindow"
+		if !tabs[wtab] {
+			cbr += ",[MINION_FILE, ../../../examples/grid.minion],[WIDGET, "
+			cbr += wtab
+			cbr += ", [PARENT, ()viewer]]"
+			tabs[wtab] = true
+		}
+		cbr += ",[WIDGET, ()viewer, [SELECT, "
+		cbr += wtab
+		cbr += "]]]"
+
+	} else if wname == "()complex" {
+
+		cbr = "[[WIDGET_PREFIX, complex]"
+		wtab := "(complex)l_MainWindow"
+		if !tabs[wtab] {
+			cbr += ",[MINION_FILE, ../../../examples/complex.minion],[WIDGET, "
+			cbr += wtab
+			cbr += ", [PARENT, ()viewer]]"
+			tabs[wtab] = true
+		}
+		cbr += ",[WIDGET, ()viewer, [SELECT, "
+		cbr += wtab
+		cbr += "]]]"
+
+	} else if strings.HasPrefix(wname, "(buttons)") {
+
+		cbr = "[[WIDGET, Output_1, [VALUE, "
+		cbr += wname
+		cbr += "]]]"
+
+	} else if strings.HasPrefix(wname, "(grid)") {
+
+		cbr = "[[WIDGET, "
+		cbr += wname
+		cbr += ", [TEXT, \""
+		cbr += wname
+		cbr += " pushed\"]]]"
+
+	} else if strings.HasPrefix(wname, "(complex)") {
+
+		cbr = "[[WIDGET, TableTotals, [VALUE, "
+		cbr += gominion.DumpString(data)
+		cbr += "]]"
+
+		if wname == "(complex)EF1" || wname == "(complex)EF4" {
+			cbr += ", [WIDGET, popup, [SHOW, "
+			cbr += wname
+			cbr += "]]"
+		}
+		cbr += "]"
+
+	} else {
+		panic("Unknown Callback" + gominion.DumpString(data))
+	}
+
 	fmt.Println("CB: " + cbr)
 	return cbr
 }
